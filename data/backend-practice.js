772f624356e3695d401f6f702eecec703440947d
Karan Verma <kverma2750@gmail.com>
2025-01-17 14:35:58 -0800

    remove these files

--- data/backend-practice.js
+++
@@ -1,8 +0,0 @@
-const xhr = new XMLHttpRequest(); //new http message
-
-xhr.addEventListener('load', () => {
-  console.log(xhr.response);
-});
-
-xhr.open('GET', 'https://supersimplebackend.dev/documentations');
-xhr.send();
